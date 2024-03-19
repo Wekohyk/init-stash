4283d5afb786c6c207c40f119a27161c40a4e7aa
huangyukai <wekohyk@gmail.com>
2024-03-19 10:28:28 +0800

    Add vue-i18n package and integrate it into the app

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import pinia from './stores';
 import persist from 'pinia-plugin-persistedstate';
 // vue-router
 import router from './router';
+// i18n
+import i18n from './lang';
 // App
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(pinia);
+app.use(i18n);
 app.use(createPinia().use(persist));
 app.use(router);
 app.mount('#app');
