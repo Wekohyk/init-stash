09437f11663919bb097d48c6fbacb9e88444b625
huangyukai <wekohyk@gmail.com>
2024-03-19 10:21:46 +0800

    Add pinia and pinia-plugin-persistedstate dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+// pinia and pinia-plugin-persistedstate
+import { createPinia } from 'pinia';
+import pinia from './stores';
+import persist from 'pinia-plugin-persistedstate';
+// App
+import App from './App.vue';
 
 const app = createApp(App);
 
-app.mount("#app");
+app.use(pinia);
+app.use(createPinia().use(persist));
+app.mount('#app');
