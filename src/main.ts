4048384414df5d32aed4bc57975b19893cfdcdf2
huangyukai <wekohyk@gmail.com>
2024-03-19 10:24:40 +0800

    Add vue-router dependency and import router in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import pinia from './stores';
 import persist from 'pinia-plugin-persistedstate';
+// vue-router
+import router from './router';
 // App
 import App from './App.vue';
 
@@ -10,4 +12,5 @@ const app = createApp(App);
 
 app.use(pinia);
 app.use(createPinia().use(persist));
+app.use(router);
 app.mount('#app');
