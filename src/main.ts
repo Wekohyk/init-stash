03f22387499676cfdb1914ae667681017ff3c3e7
huangyukai <wekohyk@gmail.com>
2024-04-17 09:33:18 +0800

    remove superfluous pinia import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue';
 // pinia and pinia-plugin-persistedstate
-import { createPinia } from 'pinia';
 import pinia from './stores';
-import persist from 'pinia-plugin-persistedstate';
 // vue-router
 import router from './router';
 // i18n
@@ -21,6 +19,5 @@ const app = createApp(App);
 
 app.use(pinia);
 app.use(i18n);
-app.use(createPinia().use(persist));
 app.use(router);
 app.mount('#app');
