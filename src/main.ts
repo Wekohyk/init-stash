6dde20fece35e296f8a9b53bb716694702a9b972
huangyukai <wekohyk@gmail.com>
2024-12-11 15:03:25 +0800

    feat: add back-to-top custom instructions

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// 自定义指令
+import backToTop from './directive/back-to-top';
 import { createApp } from 'vue';
 // pinia and pinia-plugin-persistedstate
 import pinia from './store';
@@ -17,7 +19,5 @@ import App from './App.vue';
 
 const app = createApp(App);
 
-app.use(pinia);
-app.use(i18n);
-app.use(router);
+app.use(pinia).use(i18n).use(router).directive('back-to-top', backToTop);
 app.mount('#app');
